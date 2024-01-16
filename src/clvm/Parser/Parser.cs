ccff439c73104e6d026b2bc217c48b6f6801fe61
Don Kackman <dkackman@gmail.com>
2024-01-15 19:25:33 -0600

    add Parser

---
+++ src/clvm/Parser/Parser.cs
@@ -0,0 +1,21 @@
+using System.Numerics;
+using System.Runtime.InteropServices;
+using chia.dotnet.bls;
+
+namespace chia.dotnet.clvm;
+
+public static class Parser
+{
+    public static Token? Next(IList<Token> tokens)
+    {
+        tokens.RemoveAt(0);
+        return tokens.Count > 0 ? tokens[0] : null;
+    }
+
+    public static void Expect(string source, IList<Token> tokens)
+    {
+        Token token = tokens[0];
+        if (Next(tokens) is null)
+            throw new Exception($"Unexpected end of source at {new Position(source, token.Index)}.");
+    }
+}
