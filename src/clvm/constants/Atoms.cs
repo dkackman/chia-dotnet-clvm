2c79591836087d52d5ba4003b34233b083bc647d
Don Kackman <dkackman@gmail.com>
2024-02-05 18:00:03 -0600

    lint

--- src/clvm/constants/Atoms.cs
+++ src/clvm/constants/Atoms.cs
@@ -4,10 +4,10 @@ namespace chia.dotnet.clvm;
 
 internal static class Atoms
 {
-    public static readonly byte[] QuoteAtom = ByteUtils.EncodeBigInt(KeywordConstants.Keywords["q"]);
-    public static readonly byte[] ApplyAtom = ByteUtils.EncodeBigInt(KeywordConstants.Keywords["a"]);
-    public static readonly byte[] FirstAtom = ByteUtils.EncodeBigInt(KeywordConstants.Keywords["f"]);
-    public static readonly byte[] RestAtom = ByteUtils.EncodeBigInt(KeywordConstants.Keywords["r"]);
-    public static readonly byte[] ConsAtom = ByteUtils.EncodeBigInt(KeywordConstants.Keywords["c"]);
-    public static readonly byte[] RaiseAtom = ByteUtils.EncodeBigInt(KeywordConstants.Keywords["x"]);
+    public static readonly byte[] QuoteAtom = KeywordConstants.Keywords["q"].EncodeBigInt();
+    public static readonly byte[] ApplyAtom = KeywordConstants.Keywords["a"].EncodeBigInt();
+    public static readonly byte[] FirstAtom = KeywordConstants.Keywords["f"].EncodeBigInt();
+    public static readonly byte[] RestAtom = KeywordConstants.Keywords["r"].EncodeBigInt();
+    public static readonly byte[] ConsAtom = KeywordConstants.Keywords["c"].EncodeBigInt();
+    public static readonly byte[] RaiseAtom = KeywordConstants.Keywords["x"].EncodeBigInt();
 }
